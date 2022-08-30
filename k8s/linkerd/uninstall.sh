698312e624ea9f279df88e89462048b06169a38e
Marcio Zacarias <mrzacarias@gmail.com>
2022-08-30 13:22:51 -0500

    Removing main ns annotation

--- k8s/linkerd/uninstall.sh
+++ k8s/linkerd/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # To remove Linkerd Viz
 linkerd viz uninstall | kubectl delete -f -
+echo -e "\033[32m√\033[0m LinkerD Viz uninstalled!"
 
 # Removing everything else
 linkerd uninstall | kubectl delete -f -
-
 echo -e "\033[32m√\033[0m LinkerD uninstalled!"
